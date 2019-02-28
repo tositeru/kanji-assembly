e8c65b2a3b0276b248b962382ca04d3a3d3b4a3d
tositeru <tositeru104@protonmail.com>
2019-02-28 20:42:10 +0900

    close #3

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template lang=pug>
   section()
-    h1 漢字組み立てパズル
-      div(class="login") ログイン/サインイン
+    h1(class="display-4 text-xs-center") 漢字組み立てパズル
+      div(class="login")
+        n-link(to='/login') ログイン/サインイン
     div(class="Ad") 広告スペース
     question(title="今日の問題")
 </template>
@@ -20,9 +21,7 @@ export default {
 
 <style lang="scss" scoped>
 h1 {
-  margin: 0px auto;
-  font-size: 72px;
-  text-align: center;
+  margin: 10px auto;
   .login {
     font-size: 32px;
     text-align: right;
