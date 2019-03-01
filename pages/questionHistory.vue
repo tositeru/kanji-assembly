e67e9bb8f367a773e0f4693cec260fa331f8d150
tositeru <tositeru104@protonmail.com>
2019-03-01 21:47:56 +0900

    close #7

---
+++ pages/questionHistory.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  v-container()
+    g-title(class="text-xs-center")
+    v-layout(justify-center)
+      v-flex()
+        h2(class="display-3 text-xs-center") 問題一覧
+    v-scroll-x-transition(mode="out-in")
+      component(:is="currentCondition" @change-status="changeStatus($event)")
+</template>
+<script>
+import History from '~/components/questionHistory/history.vue'
+import SelectedQuestion from '~/components/questionHistory/selectedQuestion.vue'
+
+export default {
+  components: {
+    history: History,
+    selectedQuestion: SelectedQuestion
+  },
+  data() {
+    return {
+      status: 'history'
+    }
+  },
+  computed: {
+    currentCondition() {
+      return this.status
+    }
+  },
+  methods: {
+    changeStatus(nextStatus) {
+      this.status = nextStatus
+    }
+  }
+}
+</script>
+<style lang="scss"scoped>
+</style>
