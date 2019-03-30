2501aa6ab0909291a88cd2ec5df5d9fbb312a80a
tositeru <tositeru104@protonmail.com>
2019-03-30 15:44:14 +0900

    write /about

---
+++ components/about/author.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+	v-card(flat)
+		v-card-text
+			| tositeruといいます。適当にゲームを作っています。
+		v-card-text
+			p ポートフォリオサイト: 
+				a(href="https://tositeru.github.io/") https://tositeru.github.io/
+			p twitter: 
+				a(href="https://twitter.com/tosi_teru") https://twitter.com/tosi_teru
+			p github: 
+				a(href="https://github.com/tositeru") https://github.com/tositeru
+		v-card-text
+			| 面白いと思われた方は支援していただけると大変うれしいです！
+			a(href="https://www.pixiv.net/fanbox/creator/34342710") pixivFANBOX
+</template>
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
