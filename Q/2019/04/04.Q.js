bc75f91e168a213cc7f7d6e1f04b7efd8770d5a8
tositeru <tositeru104@protonmail.com>
2019-04-03 17:30:13 +0900

    add Questions

--- Q/2019/04/04.Q.js
+++ Q/2019/04/04.Q.js
@@ -7,20 +7,23 @@ module.exports = {
    * @param {object} utils 便利関数を集めたもの
    */
   async run(database, utils) {
+    const date = __filename.match(/(\d{4})\/(\d{2})\/(\d{2})/)
     // 問題追加
     await utils.createQuestion({
-      date: moment.tz('2019-04-03 00:00:00', 'Asia/Tokyo').format(),
+      date: moment
+        .tz(`${date[1]}-${date[2]}-${date[3]} 00:00:00`, 'Asia/Tokyo')
+        .format(),
       dateId: 0,
       type: 1,
       description: '次の線から成る漢字はなんでしょう？',
-      answers: ['心'],
-      hints: ['特徴的な形です', '○技体']
+      answers: ['川'],
+      hints: ['世界中にあります', '○の字に寝る', '魚が泳いでいます']
     })
 
     // 漢字を追加
     await utils.createKanjiStrokes({
-      kanji: '心',
-      strokes: [18, 3, 20, 20]
+      kanji: '川',
+      strokes: [19, 17, 17]
     })
   }
 }
