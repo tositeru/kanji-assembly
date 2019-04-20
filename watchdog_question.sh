bbaab659a56c25a01e30279912c52a95ce9f1006
tositeru <tositeru104@protonmail.com>
2019-04-20 15:58:40 +0900

    rename shell script

--- watchdog_question.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git log -n 1 --stat | \
- /bin/sed -n /\|/p | \
- /bin/sed -n -E "s/^.+([0-9]{4}\/[0-9]{2}\/[0-9]{2})\.Q\.js.+$/echo node Q\/createQ \-e production \1/p" | \
- /bin/bash
